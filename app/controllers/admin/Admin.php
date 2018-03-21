faecdbe8908620768e0a9c54a2af6d81f982ee24
FerreWagner <18408229270@163.com>
2018-03-21 16:47:07 +0800

    helper function config

--- app/controllers/admin/Admin.php
+++ app/controllers/admin/Admin.php
@@ -18,6 +18,12 @@ class Admin extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    
 	public function index()
 	{
 		$this->load->view('admin/index.html');

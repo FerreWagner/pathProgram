5b90acbb49096e929a10f9bc56b23d90ac816ad0
FerreWagner <18408229270@163.com>
2018-03-23 10:44:32 +0800

    fixed default routes bug

--- app/controllers/home/Home.php
+++ app/controllers/home/Home.php
@@ -18,6 +18,16 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    
+//     public function __construct()
+//     {
+//         parent::__construct();
+//         $this->load->helper('url');
+    
+//         //加载公共主题
+    
+//     }
+    
 	public function index()
 	{
 		$this->load->view('home/index.html');

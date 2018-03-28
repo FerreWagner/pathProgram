922d48de2d8f3568c13c1c1215260b45870f97d3
FerreWagner <18408229270@163.com>
2018-03-28 13:56:58 +0800

    admin list success

--- app/controllers/admin/User.php
+++ app/controllers/admin/User.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class User extends CI_Controller 
+{
 
     public function __construct()
     {
@@ -12,8 +13,19 @@ class Login extends CI_Controller {
         
     }
     
-	public function index()
+    /**
+     * 登录页
+     */
+	public function login()
 	{
 		$this->load->view('admin/login.html');
 	}
+	
+	public function admin()
+	{
+	    $this->load->model('User_model');
+	    $data['user_list'] = $this->User_model->get_user_list();
+	    $this->load->view('admin/user.html', $data);
+	}
+	
 }
---
+++ app/models/User_model.php
@@ -0,0 +1,73 @@
+<?php 
+class User_model extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->load->database();
+        $this->load->helper('url');
+        // Your own constructor code
+    }
+
+    
+    /**
+     * user列表显示数据
+     */
+    public function get_user_list()
+    {
+        $query = $this->db->get('admin');
+        return $query->result();
+    }
+    
+    /**
+     * user查询edit页面数据
+     * @param unknown $id
+     */
+    public function user_edit($id)
+    {
+        $query = $this->db->get_where('admin', ['id' => $id]);
+        return $query->result_array();
+    }
+    
+    /**
+     * user数据修改
+     */
+    public function user_updates()
+    {
+        $update_data = [
+            'title' => $this->input->post('title'),
+            'sort'  => $this->input->post('sort'),
+            'desc'  => $this->input->post('desc'),
+        ];
+        
+        return $this->db->update('user', $update_data, ['id' => $this->input->post('id')]);
+    }
+    
+    /**
+     * user数据添加
+     */
+    public function user_adds()
+    {
+        $add_data = [
+            'title' => $this->input->post('title'),
+            'sort'  => $this->input->post('sort'),
+            'desc'  => $this->input->post('desc'),
+        ];
+        
+        return $this->db->insert('admin', $add_data);
+    }
+    
+    /**
+     * user数据删除 TODO软删除
+     * @param unknown $id
+     */
+    public function user_deletes($id)
+    {
+        return $this->db->delete('admin', ['id' => $id]);
+    }
+
+}
+
+
+?>
